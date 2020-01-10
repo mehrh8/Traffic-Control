ae789933f84993f2c8ac7a66a2c277bd2217a07c
kntu-drive <46350297+amirvedadiyan@users.noreply.github.com>
2020-01-10 22:09:16 +0330

    Create Movable abstrac class

---
+++ src/main/java/ir/ac/kntu/Movable/Movable.java
@@ -0,0 +1,26 @@
+package java.ir.ac.kntu.Movable;
+
+
+import javafx.geometry.Point2D;
+
+public abstract class Movable {
+
+    private Point2D location;
+    private double rotateDegree;
+
+    public Point2D getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point2D location) {
+        this.location = location;
+    }
+
+    public double getRotateDegree() {
+        return rotateDegree;
+    }
+
+    public void setRotateDegree(double rotateDegree) {
+        this.rotateDegree = rotateDegree;
+    }
+}
