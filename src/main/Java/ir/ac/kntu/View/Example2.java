16b947f02d40e7ad9c25312db40025a05ba56b9b
Mehrshad Hosseini <mehrshad.hosseini28@gmail.com>
2020-01-15 14:27:24 +0330

    Add Maps/Example.txt and Create a map: Map1.txt

--- src/main/Java/ir/ac/kntu/View/Example2.java
+++ src/main/Java/ir/ac/kntu/View/Example2.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Example2 extends Application {
@@ -15,10 +16,13 @@ public class Example2 extends Application {
     @Override
     public void start(Stage stage) {
         Group root = new Group();
-        Scene scene = new Scene(root, 1000,1000, Color.rgb(0, 0, 10));
+        Scene scene = new Scene(root, 1500,1000, Color.rgb(0, 0, 10));
         stage.setScene(scene);
         stage.setTitle("Test");
         stage.setResizable(false);
         stage.show();
+        Rectangle r = new Rectangle(300,300);
+        r.setFill(Color.RED);
+        root.getChildren().add(r);
     }
 }
